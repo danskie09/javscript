f27a5c9ddc4b65e32451f67b0f214ae50cc56dba
danskie09 <banaybanaydaniel09@gmail.com>
2025-04-22 13:34:11 +0800

    implement even/odd check

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-function calculate() {
-    let length = parseFloat(document.getElementById("length").value)
-
-    let width = parseFloat(document.getElementById("width").value)
+let num = 300
 
 
-    if (isNaN(length && width)) {
-        alert("Please enter valid numbers " + length + " "+ width + "are not valid numbers")
-    }
-
-    else {
-        let area = length * width
-
-
-        alert("The area of a rectangle is " + area)
-    }
+if (num % 2 === 0 ) {
+    console.log("its even")
+    
 }
 
-
-
-
-
-
+else {
+    console.log("its odd")
+}
