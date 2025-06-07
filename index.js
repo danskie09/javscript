e6eef09f0010e1c196c08c5256cac3a1a8569fc5
danskie09 <banaybanaydaniel09@gmail.com>
2025-06-07 15:06:04 +0800

    update grocery list by replacing 'Eggs' with 'Butter' and adding 'Laundry Soap'

--- index.js
+++ index.js
@@ -1,31 +1,10 @@
+let groceryList = ["🥛 Milk", "🥑 Avocado", "🥚 Eggs", "🍞 Bread"];
+
+
+groceryList[2] =  "Butter"
+
+groceryList.push("Laundry Soap")
 
 
 
-function fizzbuzz(input){
-    for(let i = 0; i <= input; i++){
-        if(i % 3 === 0 && i % 5 === 0){
-            console.log('Fizz buzz')
-
-        }
-        else if(i % 3 === 0 ){
-            console.log('Fizz ')
-
-        }
-
-        else if(i % 5 === 0){
-            console.log('Buzz')
-
-        }
-
-        else{
-            console.log(i)
-        }
-
-   
-        
-
-    }
-}
-
-
-fizzbuzz(10)
+console.log(groceryList)
