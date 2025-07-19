4e7acba2629a137fa7012b0db1443619321252c2
Daniel Banaybanay <banaybanaydaniel09@gmail.com>
2025-07-19 17:50:38 +0800

    Drake Manipulation

--- uppercase-lowercase.js
+++ uppercase-lowercase.js
@@ -1,6 +1,5 @@
 let word = "Daniel"
-let arr = word.split("");
-console.log(arr)
+
 
 
 let result = ''
@@ -8,12 +7,14 @@ let result = ''
 
 
 for(let i = 0; i < word.length; i++){
-    console.log(word[i])
+   
     if(i % 2 == 0){
         result += word[i].toUpperCase()
+        console.log(result)
 
     }else{
         result += word[i].toLowerCase()
+        console.log(result)
     }
 
     
