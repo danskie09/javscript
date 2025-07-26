958a737fba217db3fb3bd1004eaa62b997db7b13
Daniel Banaybanay <banaybanaydaniel09@gmail.com>
2025-07-26 20:15:47 +0800

    using the rest operator

---
+++ intercontinental-vacation.js
@@ -0,0 +1,12 @@
+const planVacation = (...country) => {
+    return country;
+}
+
+
+let destinationOne = planVacation("Croatia","Philippines","Malaysia")
+let destinationTwo = planVacation("Bali", "Switzerland")
+
+
+
+console.log(destinationOne)
+console.log(destinationTwo)
